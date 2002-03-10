e8627e2a5127283fb5930c942dbf921d3f8b73fc
Jaakko Heinonen <jheinonen@users.sourceforge.net>
2002-03-10 20:54:29 +0000

    2nd try to fix a warning2nd try to fix a warning

--- getopt.c
+++ getopt.c
@@ -651,18 +651,18 @@ int
 				else
 				{
 					if (opterr)
+					{
 						if (argv[optind - 1][1] == '-')
 							/* --option */
 							fprintf(stderr,
 								_("%s: option `--%s' doesn't allow an argument\n"),
 								argv[0], pfound->name);
 						else
-						{
 							/* +option or -option */
 							fprintf(stderr,
 								_("%s: option `%c%s' doesn't allow an argument\n"),
 								argv[0], argv[optind - 1][0], pfound->name);
-						}
+					}
 
 					nextchar += strlen(nextchar);
 
